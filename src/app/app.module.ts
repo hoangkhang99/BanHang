51bda82317289869df7d1ca8bd0657eb4ebc6769
unknown <minhkhang3019@gmail.com>
2020-10-12 16:33:28 +0700

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { MainModule } from './main/main.module';
+import {FooterComponent} from './shares/footer/footer.component'
+import { MenuComponent } from './shares/menu/menu.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProductModule } from './product/product.module';
+import { CustomerModule } from './customer/customer.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    MenuComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MainModule,
+    AppRoutingModule,
+    ProductModule,
+    CustomerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
